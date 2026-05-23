3da804721aa2ff7c0520780daa103ee77a445e40
pawelbyczynski-oss <pawelbyczynski@gmail.com>
2026-05-23 23:46:11 +0100

    Set local Supabase config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.VALORA_CONFIG = {
-  "SUPABASE_URL": "",
-  "SUPABASE_ANON_KEY": ""
+  "SUPABASE_URL": "https://ghcwjuckcgqdnmjwmjcj.supabase.co",
+  "SUPABASE_ANON_KEY": "sb_publishable_Epuk0nLQ9MuXty0eRm5KAw_QXRrsod5"
 };
